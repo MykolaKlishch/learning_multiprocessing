69bad811d1b30d143b0d87243017db27e0f02464
Mykola Klishch <mykola.klishch.devmarks@gmail.com>
2020-10-05 14:02:59 +0300

    ProcessPoolExecutor introduced

--- start.py
+++ start.py
@@ -1,28 +1,32 @@
-import multiprocessing
+import concurrent.futures
 import time
 
 
 def do_something(seconds=1):
-    print(f'Sleeping for {seconds} second(1)...')
+    print(f'Sleeping for {seconds} second(s)...')
     time.sleep(seconds)
-    print('Done Sleeping...')
+    return 'Done Sleeping...'
 
 
 if __name__ == "__main__":
 
     start = time.perf_counter()
 
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        f1 = executor.submit(do_something, 1)
+        print(f1.result())
+
     processes = []
 
-    for _ in range(10):
-        p = multiprocessing.Process(
-            target=do_something, args=(1.5,)
-        )
-        p.start()
-        processes.append(p)
-
-    for process in processes:
-        process.join()
+    # for _ in range(10):
+    #     p = multiprocessing.Process(
+    #         target=do_something, args=(1.5,)
+    #     )
+    #     p.start()
+    #     processes.append(p)
+    #
+    # for process in processes:
+    #     process.join()
 
     finish = time.perf_counter()
 
